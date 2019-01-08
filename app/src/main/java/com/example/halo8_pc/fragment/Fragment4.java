e5e61e211f489e0a36226fc9ad5c147a91386e07
in6802 <44351650+in6802@users.noreply.github.com>
2019-01-09 04:28:32 +0900

    fragment, animation, button press anim

---
+++ app/src/main/java/com/example/halo8_pc/fragment/Fragment4.java
@@ -0,0 +1,18 @@
+package com.example.halo8_pc.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment4 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment4, container, false);
+    }
+}
